feeff4a5e4f297a71f576567e4a426430bf75c71
au-rea <canron826@naver.com>
2023-02-16 17:16:56 +0900

    2023-02-16 17:12 recipe 기능들 -cm(gyugeun)

---
+++ src/main/java/com/multi/smore/recipe/model/vo/Recipe.java
@@ -0,0 +1,69 @@
+package com.multi.smore.recipe.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+	private int rcpNo;
+	private String rcpNm;
+	private String rcpWay2;
+	private String rcpPat2;
+	private String infoWgt;
+	private String infoEng;
+	private String infoCar;
+	private String infoPro;
+	private String infoFat;
+	private String infoNa;
+	private String hashTag;
+	private String attFileNoMain;
+	private String attFileNoMk;
+	private String rcpPartsDtls;
+	private String manual01;
+	private String manualImg01;
+	private String manual02;
+	private String manualImg02;
+	private String manual03;
+	private String manualImg03;
+	private String manual04;
+	private String manualImg04;
+	private String manual05;
+	private String manualImg05;
+	private String manual06;
+	private String manualImg06;
+	private String manual07;
+	private String manualImg07;
+	private String manual08;
+	private String manualImg08;
+	private String manual09;
+	private String manualImg09;
+	private String manual10;
+	private String manualImg10;
+	private String manual11;
+	private String manualImg11;
+	private String manual12;
+	private String manualImg12;
+	private String manual13;
+	private String manualImg13;
+	private String manual14;
+	private String manualImg14;
+	private String manual15;
+	private String manualImg15;
+	private String manual16;
+	private String manualImg16;
+	private String manual17;
+	private String manualImg17;
+	private String manual18;
+	private String manualImg18;
+	private String manual19;
+	private String manualImg19;
+	private String manual20;
+	private String manualImg20;
+	private int isLike;
+	private int likeCount;
+	private int reviewCount;
+	private int isClip;
+}
---
+++ src/main/java/com/multi/smore/recipe/model/vo/RecipeClip.java
@@ -0,0 +1,13 @@
+package com.multi.smore.recipe.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeClip {
+	private int rcpNo;
+	private int memNo;
+}
